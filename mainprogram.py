fdf784c05993c6f704bda3844de005be2f90f107
Laith-18 <18LHanson@salesian.surrey.sch.uk>
2024-11-19 12:09:22 +0000

    Add files via upload

---
+++ mainprogram.py
@@ -0,0 +1,12 @@
+from animalfile import animals
+
+name1 = "ayan"
+pet1 = "dog"
+sound1 = "yap"
+
+req_animal = animals(name1,pet1,sound1)
+
+print(req_animal.speak())
+
+print(req_animal.pet_info())
+
