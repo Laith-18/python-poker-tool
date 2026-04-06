f83d916a5776f4d4206e72d0824b47ec7022fcce
Laith Hanson <laith_hanson@icloud.com>
2026-04-06 19:17:35 +0100

    Implemented python script that loads card images from folder.

---
+++ utilities/card_loader.py
@@ -0,0 +1,10 @@
+def get_card_image_from_file(card):
+
+    #Mapping for cards
+    ranks = {'2': '2', '3': '3', '4': '4', '5': '5', '6': '6', '7': '7', '8': '8', '9': '9', '10': '10', 'j': 'jack', 'q': 'queen', 'k': 'king', 'a': 'ace'}
+    suits = {'c': 'clubs', 'd': 'diamonds', 'h': 'hearts', 's': 'spades'}
+
+    rank = card[0]
+    suit = card[1]
+
+    return f"cards/{ranks[rank]}_of_{suits[suit]}.png" #Load the image
